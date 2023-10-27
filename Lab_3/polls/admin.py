d60af8d6709d6906e83526a81acef7facabaa922
PDondalski <piotr.dondalski@student.uwm.edu.pl>
2023-10-27 13:27:09 +0200

    Lab_3 - Zadania
    Wykonane zadania z Labu 3.

---
+++ Lab_3/polls/admin.py
@@ -0,0 +1,32 @@
+from django.contrib import admin
+from django.contrib.admin import ModelAdmin
+
+from .models import Team, Person, Question, Choice, Stanowisko, Osoba
+# Register your models here.
+admin.site.register(Team)
+admin.site.register(Person)
+admin.site.register(Question)
+admin.site.register(Choice)
+admin.site.register(Stanowisko)
+admin.site.register(Osoba)
+
+class OsobaAdmin(admin.ModelAdmin):
+    list_display = ['imie', 'nazwisko', 'disp_stanowisko', 'data_dodania']
+    list_filter = ['stanowisko', 'data_dodania']
+    readonly_fields = ['data_dodania']
+
+    @admin.display(description='Stanowisko')
+    def disp_stanowisko(self, obj):
+        return f'{obj.stanowisko.nazwa} ({obj.stanowisko.id})'
+
+class StanowiskoAdmin(admin.ModelAdmin):
+    list_display = ('nazwa', 'opis')
+    search_fields = ('nazwa',)
+
+
+admin.site.unregister(Osoba)
+admin.site.unregister(Stanowisko)
+admin.site.register(Stanowisko, StanowiskoAdmin)
+admin.site.register(Osoba, OsobaAdmin)
+
+
---
+++ Lab_3/polls/models.py
@@ -0,0 +1,71 @@
+from django.db import models
+
+
+# Create your models here.
+
+# deklaracja statycznej listy wyboru do wykorzystania w klasie modelu
+MONTHS = models.IntegerChoices('Miesiace', 'Styczeń Luty Marzec Kwiecień Maj Czerwiec Lipiec Sierpień Wrzesień Październik Listopad Grudzień')
+
+SHIRT_SIZES = (
+        ('S', 'Small'),
+        ('M', 'Medium'),
+        ('L', 'Large'),
+        ('XL', 'Extra Large'),
+    )
+
+
+class Plec(models.IntegerChoices):
+    MEZCZYZNA = 1, "Mezczyzna"
+    KOBIETA = 2, "Kobieta"
+    INNA = 3, "Inna"
+
+class Team(models.Model):
+    name = models.CharField(max_length=60)
+    country = models.CharField(max_length=2)
+
+    def __str__(self):
+        return f"{self.name}"
+
+
+class Person(models.Model):
+
+    name = models.CharField(max_length=60)
+    shirt_size = models.CharField(max_length=2, choices=SHIRT_SIZES, default=SHIRT_SIZES[0][0])
+    month_added = models.IntegerField(choices=MONTHS.choices, default=MONTHS.choices[0][0])
+    team = models.ForeignKey(Team, null=True, blank=True, on_delete=models.SET_NULL)
+
+    def __str__(self):
+        return self.name
+
+class Question(models.Model):
+    question_text = models.CharField(max_length=200)
+    pub_date = models.DateTimeField('date published')
+
+
+class Choice(models.Model):
+    question = models.ForeignKey(Question, on_delete=models.CASCADE)
+    choice_text = models.CharField(max_length=200)
+    votes = models.IntegerField(default=0)
+
+
+class Stanowisko(models.Model):
+    nazwa = models.CharField(null=False, blank=False, max_length=30)
+    opis = models.CharField(blank=True, max_length=200)
+
+    def __str__(self):
+        return self.nazwa
+
+
+class Osoba(models.Model):
+    imie = models.CharField(null=False, blank=False, max_length=20)
+    nazwisko = models.CharField(null=False, blank=False, max_length=25)
+    plec = models.IntegerField(choices=Plec.choices)
+    stanowisko = models.ForeignKey(Stanowisko, on_delete=models.CASCADE)
+    data_dodania = models.DateField(auto_now_add=True)
+
+    class Meta:
+        ordering = ["nazwisko"]
+
+    def __str__(self):
+        return f'{self.imie} {self.nazwisko}'
+
