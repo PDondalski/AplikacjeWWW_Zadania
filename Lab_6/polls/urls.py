48f5eaa797632cc9ca4070cd7bb089810d1652c5
PDondalski <piotr.dondalski@student.uwm.edu.pl>
2023-11-20 00:00:07 +0100

    Lab_6 - Zadania
    Wykonane zadania z labu 6.

---
+++ Lab_6/polls/urls.py
@@ -0,0 +1,23 @@
+from django.urls import path, include
+from rest_framework.urlpatterns import format_suffix_patterns
+from . import views
+
+urlpatterns = [
+    #path('osobas/', views.OsobaList.as_view()),
+    #path('osobas/<int:pk>/', views.OsobaDetail.as_view()),
+    #path('', views.index, name='index'),
+    path('osobas/', views.osoba_list),
+    path('osobas/<int:pk>/', views.osoba_detail),
+    path('osobas/update/<int:pk>/', views.osoba_update),
+    path('osobas/delete/<int:pk>/', views.osoba_delete),
+    #path('osobas/add/', views.osoba_add),
+    #path('osobas_filtered/<slug:slug>', views.osoba_list_filtered),
+    path('stanowiskos/<int:pk>/', views.stanowisko_detail),
+    path('stanowiskos/', views.stanowisko_list),
+    path('stanowiskos/add/', views.stanowisko_add),
+    path('stanowisko/<int:pk>/members', views.StanowiskoList.as_view(), name='StanowiskoList'),
+    #path('api-auth/', include('rest_framework.urls')),
+]
+
+urlpatterns = format_suffix_patterns(urlpatterns)
+
